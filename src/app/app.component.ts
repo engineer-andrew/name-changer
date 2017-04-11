3c33cd7dc508840e8389198d01ac76610f1c6726
Andrew Webster <andrew.webster@heatsoftware.com>
2017-04-11 16:06:42 -0700

    Initial check-in; Just needs error handling and name dictionaries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tippy Tinkletrousers\' Name Changer!';
+  currentName: string;
+  results: Array<string> = [];
+  first = {
+    'R': 'Loopy'
+  };
+  beginningLast = {
+    'K': 'Potty'
+  };
+  endingLast = {
+    'R': 'Buns'
+  };
+
+  translate() {
+    const currentNameArray = this.currentName.split(' ');
+    const firstName = currentNameArray[0];
+    const lastName = currentNameArray[currentNameArray.length - 1];
+    let letter = firstName[0].toUpperCase();
+    const newFirst = this.first[letter];
+    letter = lastName[0].toUpperCase();
+    const newBeginningLast = this.beginningLast[letter];
+    letter = lastName[lastName.length - 1].toUpperCase();
+    const newEndingLast = this.endingLast[letter].toLowerCase();
+
+    const newName = newFirst + ' ' + newBeginningLast + newEndingLast;
+
+    const message = this.currentName + '\'s new name is ' + newName;
+    this.results.unshift(message);
+  }
 }
